d9bc51cd3568051374da50cea390b04589fa51f9
imranshahzad17 <imranshahzad3454@gmail.com>
2024-03-13 17:08:55 +0500

    Done

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// Mode
-
 let toggle = document.querySelector("#toggle");
 let body = document.querySelector("body");
 let light = false;
@@ -11,21 +9,3 @@ toggle.addEventListener("click",function(){
         body.style.backgroundColor = "black";
     }
 });
-
-// =========================================================================================
-
-//Counter
-
-let count = 0;
-let count2 = document.querySelector("#count");
-let Sum = document.querySelector(".ADD");
-let Sub = document.querySelector(".SUB");
-
-Sum.addEventListener("click",function(){
-    count++;
-   count2.innerHTML = count;
-})
-Sub.addEventListener("click",function(){
-    count--;
-   count2.innerHTML = count;
-})
